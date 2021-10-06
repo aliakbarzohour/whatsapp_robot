1fb180d5398c540095dba713391dfef2606ad098
aliakbarzohour <aliakbar.zohour@gmail.com>
2021-10-06 15:29:01 +0330

    configure selenium for linux and added colorama library

--- whatsapp_robot.py
+++ whatsapp_robot.py
@@ -1,25 +1,27 @@
 # hey im aliakbar zohour welcome to my project 
 
 # imports
+# pip install selenium
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from colorama import Fore,init
 
 # initing 
+init()
 driver = webdriver.Firefox()
 driver.get('https://web.whatsapp.com/')
 # start coding 
-name = input('Enter Name : ')
-msg = input('Enter Masage : ')
-count = int(input('How Many times ??? : '))
+name = input(Fore.YELLOW+' [*] '+Fore.WHITE+'Enter Name : ')
+msg = input(Fore.YELLOW+' [*] '+Fore.WHITE+'Enter Masage : ')
+count = int(input(Fore.GREEN+' [?] '+Fore.WHITE+'How Many times ??? : '))
 
-input('Enter Anything . . .')
-
+input(Fore.RED+' [~] '+Fore.WHITE+'Enter Anything . . .')
 # selenium 
-
 # xpath relative
 user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
 user.click()
 # massage box selection 
-msg_box = driver.find_element_by_class_name('_13NKt ')
+msg_box = driver.find_element_by_class_name('_13NKt')
 # loop for send massages 
 for i in range(count):
     msg_box.send_keys(msg)
